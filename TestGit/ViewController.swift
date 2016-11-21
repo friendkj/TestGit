a2a4ac427e6d6807e21d88ab0701bd8b0444fa51
jun kim <friendkj@jun-kim-imac.local>
2016-11-21 11:03:15 +0900

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by jun kim on 2016. 11. 21..
+//  Copyright © 2016년 finecore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
